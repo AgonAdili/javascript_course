f32b6f44022a8f4a7b7fb9fa13608a1f71b4848e
Agon Adili <agon.adili@gmail.com>
2025-03-22 12:56:21 +0100

    Arithmetic Operators

--- Lesson 1/main.js
+++ Lesson 1/main.js
@@ -7,3 +7,36 @@ var _vlera = 'Digital School';
 // Deklarimi qe nuk lejohet
 // var 123 = 10;
 // var *ds = 320;
+
+
+// Arithmetic Operators
+
+var a = 50;
+var b = 25;
+var c = a + b;
+console.log('Shuma e variablave a dhe b eshte', c);
+
+var d = c - b;
+console.log('Ndryshimi i variablave c dhe b eshte', d);
+
+var e = a / b;
+console.log('Heresi i variablave a dhe b eshte', e);
+
+var f = e * c;
+console.log('Prodhimi i variablave e dhe c eshte', f);
+
+var g = 10;
+var h = 4;
+var i = g % h;
+console.log('Mbetjen nga pjestimi i variablave g dhe h eshte', i);
+
+var j = 99;
+j++;
+console.log('Variabla j =', j);
+
+var k = 100;
+k--;
+console.log('Variabla k =', k);
+
+var l = 20;
+console.log('Variabla l ne katror eshte', l ** 2);
