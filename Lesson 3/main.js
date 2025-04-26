7158861bfc752c7281b8128def65d3423786c602
Agon Adili <agon.adili@gmail.com>
2025-04-26 10:06:41 +0200

    Lesson 3 ended

--- Lesson 3/main.js
+++ Lesson 3/main.js
@@ -14,26 +14,54 @@ button1.onclick = function(){
 
 
 // Event listeners
-var button2 = document.getElementById('btn_2');
+// var button2 = document.getElementById('btn_2');
 
-button2.addEventListener('click', function(){
-    alert("DON'T CLICK !!");
-});
+// button2.addEventListener('click', function(){
+//     alert("DON'T CLICK !!");
+// });
 
-var button_3 = document.getElementById('btn_3');
+// var button_3 = document.getElementById('btn_3');
 
-function function_1(){
-    console.log('Function 1 has been executed!');
+// function function_1(){
+//     console.log('Function 1 has been executed!');
+// }
+
+// function function_2(){
+//     console.log('Function 2 has been executed!');
+// }
+
+// function function_3(){
+//     console.log('Function 3 has been executed!');
+// }
+
+// button_3.addEventListener('mouseover', function_1);
+// button_3.addEventListener('mouseleave', function_2);
+// button_3.addEventListener('mousewheel', function_3);
+
+// Styling using JS
+var ds_button = document.getElementById('btn_1');
+var ds_text = document.getElementById('text_1');
+var hide_button = document.getElementById('hide_btn');
+
+ds_button.onclick = function(){
+    ds_text.style.display = 'block';
+    ds_text.style.color = 'red';
+    ds_text.style.backgroundColor = 'lightgrey';
+    ds_text.style.textAlign = 'center';
+    ds_text.style.fontSize = '100px';
+    ds_text.style.padding = '20px';
+    ds_text.style.transitionTimingFunction = 'linear';
+    ds_text.style.transition = '1s';
 }
 
-function function_2(){
-    console.log('Function 2 has been executed!');
+ds_text.onmouseover = function(){
+    ds_text.style.cssText = "color: blue; background-color: yellow; text-align: center; font-size: 100px; padding: 20px;";
 }
 
-function function_3(){
-    console.log('Function 3 has been executed!');
+ds_button.onmouseleave = function(){
+    ds_text.setAttribute("class", "websiteAnimation");
 }
 
-button_3.addEventListener('mouseover', function_1);
-button_3.addEventListener('mouseleave', function_2);
-button_3.addEventListener('mousewheel', function_3);
+hide_button.onclick = function(){
+    ds_text.setAttribute("class", "hideClass");
+}
