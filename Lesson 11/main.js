5c557d708210d0c947045e85494f57ffda961fc9
Agon Adili <agon.adili@gmail.com>
2025-10-07 19:44:19 +0200

    Lesson 11

---
+++ Lesson 11/main.js
@@ -0,0 +1,23 @@
+// $(window).ready(function() {
+//     alert('Hello World!');
+// });
+
+// $('#btn').click(function() {
+    // var newImage = $('<img>', {
+    //   src: 'image.jpg',
+    //   class: 'dynamic-image' 
+    // });
+    
+//     console.log($('#h1').text());
+//     $('#h1').text('Hello jQuery!');
+//     $('#h1').append('&nbsp;Some extra text.');
+//     $('#h1').css('color', 'red');
+//     // $('#h1').append(newImage);
+// });
+
+// $("#button").click(function() {
+//     $(this).addClass('clicked');
+//     })
+//     .find('span')
+//     .attr('title', 'I am a tooltip');
+
